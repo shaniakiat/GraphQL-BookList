4ec5235d7574a6dcd489a0414ac268c80b1c4976
shaniakiat <kiat.shania@gmail.com>
2020-05-16 14:00:22 -0400

    queries & styling

--- server/app.js
+++ server/app.js
@@ -2,9 +2,14 @@ const graphqlHTTP = require("express-graphql");
 const express = require("express");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
+//allow cross-origin requests
+app.use(cors());
+
+//connect to mongodb database
 mongoose.connect(
   "mongodb+srv://shaniakiat:ponpon@cluster0-9uqh9.mongodb.net/test?retryWrites=true&w=majority"
 );
